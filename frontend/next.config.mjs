72d7eaf09a302f2303a8c5f142b88ba6fd03a078
RajParida <rajparida6075@gmail.com>
2026-07-18 22:04:00 +0530

    renamed the next.config

---
+++ frontend/next.config.mjs
@@ -0,0 +1,41 @@
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
